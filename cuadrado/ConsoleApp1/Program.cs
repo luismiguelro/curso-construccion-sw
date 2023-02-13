d5f17d175db1bf37539e08c2fce3aa961942e024
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2023-02-13 07:35:35 -0500

    clase 13/02/2023

---
+++ cuadrado/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //variables
+            int lado,superficie;
+            string linea;
+
+            // imprimir
+            Console.Write("Digita un lado del cuadrado: ");
+            linea = Console.ReadLine();
+
+            lado = int.Parse(linea);
+            superficie = (int)Math.Pow(lado,2);
+            Console.Write("La superficie del cuadrado es: " + superficie );
+
+            Console.ReadKey();
+
+            
+
+        }
+    }
+}
---
+++ pagoHoras/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿/* Librerias para la creacion de apps de consola*/
+using System;
+using System.Collections.Generic;
+using System.Linq; // a nivel bd
+using System.Text;
+using System.Threading.Tasks; // tareas a programar
+
+
+namespace pagoHoras
+{
+    internal class Program
+    {
+        // clase principal
+        static void Main(string[] args)
+        {
+            // variables: al ser consola por defecto lo toma como string
+            int horasTrabajadas;
+            float costoHora, sueldo;
+
+            string linea;
+
+            // Imprimir
+            Console.Write("Digita las horas trabajadas: ");
+
+            //obtener valor
+            linea = Console.ReadLine();
+
+            // realizar parseo
+            horasTrabajadas = int.Parse(linea);
+
+            Console.Write("Digita el valor de la hora: ");
+            linea = Console.ReadLine();
+            costoHora = float.Parse(linea);
+
+            // operacion
+            sueldo = horasTrabajadas * costoHora;
+
+            //imprimir line
+            Console.WriteLine("Total sueldo a cancelar: "+ sueldo);
+        
+            // para que no se cierre en la ejecución
+            Console.ReadKey();
+
+            
+        }
+    }
+}
