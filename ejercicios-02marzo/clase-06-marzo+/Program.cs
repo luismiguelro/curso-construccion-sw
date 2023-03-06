29f987aca603d6b8daed3480704875bc5841e8ff
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2023-03-06 07:36:46 -0500

    clase y ejercicios 06/03/2023

---
+++ ejercicios-02marzo/clase-06-marzo+/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clase_06_marzo_23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //EJEMPLO CLASE 1
+
+            // variables
+            string linea;
+            int valor;
+
+            do
+            {
+                // solicitar digito y realizar parseo
+                Console.WriteLine("Digite un numero entre 0 y 999");
+                linea= Console.ReadLine();
+                valor = int.Parse(linea);
+
+                // validar si tiene 3 digitos
+                if (valor >=100 && valor <=999)
+                {
+
+                    Console.WriteLine($"Tiene 3 digitos: {valor}\n");
+
+                } else
+                    // validar si tiene 2 digitos
+
+                    if (valor >=10 && valor<100){
+                    Console.WriteLine($"Tiene 2 digitos: {valor}\n");
+
+                } else
+                    // validar si tiene 1 digito
+
+                    if (valor < 10)
+                {
+                    Console.WriteLine($"Tiene un solo digito: {valor}\n");
+
+                }
+                // mensaje por si se ingresa un numero mayor a 999
+                else
+                {
+                    Console.WriteLine($"{valor}: Fuera de rango...\n");
+                }
+
+            } while (valor != 0);
+            */
+
+            /* Ejemplo 2
+
+            // variables
+            string linea;
+            int suma=0, promedio=0, contador = 0;
+            int valor;
+            do
+            {
+
+
+                // solicitar digito y realizar parseo
+
+                
+                Console.WriteLine($"Digite la nota #{contador}");
+                linea = Console.ReadLine();
+                valor = int.Parse(linea);
+
+                if (valor != 0)
+                {
+                    suma = suma + valor;
+                    contador++;
+                }
+            } while(valor !=0);
+            if (contador != 0)
+            {
+                promedio= suma/contador;
+                Console.WriteLine($"Promedio{promedio}\n");
+            }
+            else
+            {
+                Console.WriteLine("No se ingresaron valores");
+            }
+
+           */
+            /*
+
+            // Ejemplo 3
+            string linea;
+            int suma = 0, promedio = 0, contador = 0;
+            int valor;
+            do
+            {
+
+
+                // solicitar digito y realizar parseo
+
+
+                Console.WriteLine($"Digite la nota #{(contador+1)}");
+                linea = Console.ReadLine();
+                valor = int.Parse(linea);
+
+                // condicion segun requerido el ejercicio
+                if (valor != 0 && valor != 9999)
+                {
+                    suma = suma + valor;
+                    contador++;
+                }
+            } while (valor != 0 && valor != 9999);
+
+            // validar si el contadoe es diferente de 0 y realizar calculo promedio
+                if (contador != 0)
+                {
+                    promedio = suma / contador;
+                
+
+                // validar si el promedio es mayor o menor que 01
+                    if ( promedio >= 0)
+                    {
+                    Console.WriteLine($"Acumulado es:{suma}");
+                    Console.WriteLine($"Valor POSITIVO...");
+                    }
+
+                    else
+                    {
+                    Console.WriteLine($"Acumulado es:{suma}");
+                    Console.WriteLine("Valor NEGATIVO...");
+
+                }
+                }
+                else
+                {
+                    Console.WriteLine("No se ingresaron valores");
+                }
+            */
+                Console.ReadKey();
+
+        }
+    }
+}
---
+++ ejercicios-24febrero/cantidad-digitos/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cantidad_digitos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Ejercicio 1
+             *
+            // variables 
+            int num1, num2;
+            string linea;
+
+            // Imprimir
+            Console.Write("Digita numero 1: ");
+
+            //obtener valor
+            linea = Console.ReadLine();
+
+            num1 = int.Parse(linea);
+
+           if (num1 <0)
+            {
+                Console.Write("Ingrese un numero positivo entre 0 y 99");
+                return;
+            }
+
+           if(num1 >=0 && num1 <=9)
+            {
+                Console.Write("El numero que ingresaste es de un dígito");
+            }
+            else
+            {
+                Console.Write("El numero que ingresaste es de dos dígitos");
+            }
+            */
+
+            /* Ejercicio: Estructura condicional notas
+
+            // variables 
+            int nota1, nota2, nota3;
+            string linea;
+
+            // Imprimir
+            Console.Write("Digita nota #1: ");
+
+            //obtener valor
+            linea = Console.ReadLine();
+
+            nota1 = int.Parse(linea);
+
+            // Imprimir
+            Console.Write("Digita nota #2: ");
+
+            //obtener valor
+            linea = Console.ReadLine();
+
+            nota2 = int.Parse(linea);
+
+            // Imprimir
+            Console.Write("Digita nota #3: ");
+
+            //obtener valor
+            linea = Console.ReadLine();
+
+            nota3 = int.Parse(linea);
+
+            int promedio = ((nota1 + nota2 + nota3) / 3);
+            if (promedio>=7)
+            {
+                Console.Write("Promocionado... Promedio: "+promedio);
+            } else
+            {
+                if (promedio>=4)
+                {
+                    Console.Write("Regular... Promedio: "+promedio);
+                } else
+                {
+                    Console.Write("Reprobado... Promedio: "+promedio);
+                }
+            }
+            */
+
+            /*
+             Ejercicio validad numero
+
+            // variables 
+            int num1;
+            string linea;
+
+            // Imprimir
+            Console.Write("Digita numero 1: ");
+
+            //obtener valor
+            linea = Console.ReadLine();
+
+            num1 = int.Parse(linea);
+
+            if (num1 < 0)
+            {
+                Console.Write("Numero negativo");
+               
+            } else
+            {
+                if (num1 > 0)
+                {
+                    Console.Write("Numero positivo");
+                }
+                else
+                {
+                    Console.Write("Null...");
+                }
+            }
+            */
+
+            int  mes;
+            string dia, año,linea;
+
+            // Imprimir
+            Console.Write("Digita día:");
+
+            //obtener valor
+            dia = Console.ReadLine();
+
+           
+
+            // Imprimir
+            Console.Write("Digita mes:");
+
+            //obtener valor
+            linea = Console.ReadLine();
+            mes = int.Parse(linea);
+
+
+            // Imprimir
+            Console.Write("Digita año:");
+
+            //obtener valor
+            año = Console.ReadLine();
+
+            /* if del ejercicio fecha trimestre
+            if(mes >=1 && mes<=3) {
+                Console.Write("Corresponde al primer trimestre.\nFecha: "+dia+"/"+mes+"/"+año);
+            }
+            else
+            {
+                Console.Write("NO corresponde al primer trimestre.\nFecha: " + dia + "/" + mes + "/" + año);
+            }
+            */
+            
+            if (mes == 12)
+            {
+                Console.Write("Corresponde a navidad.\nFecha: " + dia + "/" + mes + "/" + año);
+            }
+            else
+            {
+                Console.Write("NO corresponde aa navidad.\nFecha: " + dia + "/" + mes + "/" + año);
+            }
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
