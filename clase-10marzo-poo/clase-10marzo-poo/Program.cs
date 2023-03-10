f306fe4b6956157ae2cc1cd304b212b7b3ad39b0
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2023-03-10 16:55:04 -0500

    Clase y ejercicios 10/03/2023

---
+++ clase-10marzo-poo/clase-10marzo-poo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clase_10marzo_poo
+{
+     class Triangulo
+    {
+        //variables
+        private int lado1,lado2,lado3;
+        public void Inicializar()
+        {
+            Console.WriteLine("Ingrese lado1");
+            lado1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese lado2");
+            lado2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese lado3");
+            lado3 = int.Parse(Console.ReadLine());
+        }
+        public void LadoMayor()
+        {
+            if(lado1>lado2 && lado1 > lado2)
+            {
+                Console.WriteLine($"Lado mayor es {lado1}");
+            } else if(lado2>lado1 && lado2 > lado3)
+            {
+                Console.WriteLine($"Lado mayor es {lado2}");
+            }
+            else if (lado3 > lado1 && lado3 > lado2)
+            {
+                Console.WriteLine($"Lado mayor es {lado3}");
+            }
+            else
+            {
+                Console.WriteLine("Es un triangulo equilatero");
+            }
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            Triangulo triangulo = new Triangulo();
+            triangulo.Inicializar();
+            triangulo.LadoMayor();
+            
+        }
+    }
+}
