95b73089b796dd2cebbb13bc66230b9e1bede994
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2023-05-04 18:48:33 -0500

    Proyecto Final CRUD - Diseño Form, Conexion BD, Clases y Consultas : 04/05/2023

--- clases_y_ejercicios/abril/clase24-abril/clase-24-abril(script sql).sql
+++ clases_y_ejercicios/abril/clase24-abril/clase-24-abril(script sql).sql
@@ -17,3 +17,24 @@ SELECT TOP (1000) [codigo_articulo]
       ,[precio]
       ,[cantidad]
   FROM [clase24_abril].[dbo].[articulo]
+------------------------------------------------------------------------------------ -- 
+-- proyecto final ----
+
+-- Crear Base Datos
+CREATE DATABASE projectFinal;
+
+CREATE TABLE contactos (
+  id INT IDENTITY(1,1) PRIMARY KEY,
+  nombre VARCHAR(50) NOT NULL,
+  apellidos VARCHAR(50) NOT NULL,
+  telefono VARCHAR(20),
+  direccion VARCHAR(100)
+);
+
+-- evento btnadd
+OpenContactDetails();
+
+private void OpenContactDetails(){
+  ContactDetails contactDetails = new ContactDetails();
+  contactDetails.ShowDialog();
+}
